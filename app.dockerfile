ed09b2728e028bbb5d75b52d765f0b87ca732c50
Dixie Philamerah Atay <dixie.atay@gmail.com>
2019-03-15 21:48:53 +0800

    Setup bootstrap vue for frontend use

--- app.dockerfile
+++ app.dockerfile
@@ -8,3 +8,12 @@ RUN docker-php-ext-configure zip --with-libzip
 RUN docker-php-ext-install pdo_mysql zip
 RUN curl --silent --show-error https://getcomposer.org/installer | php && \
     mv composer.phar /usr/local/bin/composer
+
+# Install Node.js
+RUN apt install -y curl software-properties-common gnupg
+RUN curl -sL https://deb.nodesource.com/setup_8.x | bash
+RUN apt-get install --yes nodejs
+RUN node -v
+RUN npm -v
+RUN npm i -g nodemon
+RUN nodemon -v
--- resources/js/app.js
+++ resources/js/app.js
@@ -17,10 +17,14 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
 // const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+// bootstrap vue
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
