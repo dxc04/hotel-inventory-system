6c201a72081f0653609b638ccc0ea2d5ff22ecc0
Dixie Philamerah Atay <dixie.atay@gmail.com>
2019-03-17 21:36:55 +0800

    Modified frontend pages - login and main app layout.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,6 +12,8 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
+    require('datatables.net-bs4');
+    require('startbootstrap-sb-admin-2/js/sb-admin-2.min');
 } catch (e) {}
 
 /**
