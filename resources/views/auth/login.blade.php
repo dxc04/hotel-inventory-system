ffe8dc9261d853de6c12d8d4454905fa83f976f5
Dixie Philamerah Atay <dixie.atay@gmail.com>
2019-03-18 16:23:34 +0800

    Fixed syntax error.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
           <div class="col-lg-6">
             <div class="p-5">
               <div class="text-center">
-                <h1 class="h4 text-gray-900 mb-4">Welcome to {{ config('app.name', 'HSG Portal') }</h1>
+                <h1 class="h4 text-gray-900 mb-4">Welcome to {{ config('app.name', 'HSG Portal') }}</h1>
               </div>
               <form method="POST" action="{{ route('login') }}">
               @csrf
