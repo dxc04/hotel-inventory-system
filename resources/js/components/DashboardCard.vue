f6ab2f64c6e15f3ec5db7714b0237de1dfd33c26
Dixie Philamerah Atay <dixie.atay@codingavenue.com>
2019-03-22 21:16:43 +0800

    Update Dashboard Cards

--- resources/js/components/DashboardCard.vue
+++ resources/js/components/DashboardCard.vue
@@ -1,25 +1,37 @@
 <template>
-    <div class="card border-left-primary shadow h-100 py-2">
-        <div class="card-body">
-            <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Title</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-                </div>
-                <div class="col-auto">
-                    <i class="fas fa-calendar fa-2x text-gray-300"></i>
+<div class="card border-left-primary shadow h-100 py-2">
+    <div class="card-body">
+        <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+                <div class="row">
+                    <div class="col-4 mr-0 text-center align-items-center">
+                        <div class="h5 mr-0 mb-0 pr-0 font-weight-bold text-gray-800" style="font-size: 50px;">8</div>
+                    </div>
+                <div class="col-5 ml-0">
+                    <div class="text-l ml-0 pl-0 font-weight-bold text-primary mb-1">
+                        <h3><slot name="title"></slot></h3></div>
                 </div>
             </div>
         </div>
+            <div class="col-auto">
+                <i class="fas fa-4x text-gray-300" :class="dashboardIcon"></i>
+            </div>
+        </div>
     </div>
+</div>
 </template>
 
 <script>
     // todo: add props card color, icon, and content
     export default {
+        props: {
+            dashboardIcon: {
+                type: String
+            }
+        },
         name: 'DashboardCard',
         mounted() {
-            console.log('Component mounted.')
+            console.log(this.testing)
         }
     }
 </script>
--- resources/js/pages/DashboardPage.vue
+++ resources/js/pages/DashboardPage.vue
@@ -10,17 +10,29 @@
             <div class="col-xl-4 col-md-6 mb-2">
                 <div class="row">
                     <div class="col-xl-11 col-md-8 mb-4">
-                        <dashboard-card></dashboard-card>
+                        <dashboard-card dashboard-icon="fa-building">
+                            <template slot="title">
+                                Rooms Checked
+                            </template>
+                        </dashboard-card>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-xl-11 col-md-8 mb-4">
-                        <dashboard-card></dashboard-card>
+                        <dashboard-card dashboard-icon="fa-box">
+                            <template slot="title">
+                                Rooms Restocked
+                            </template>
+                        </dashboard-card>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-xl-11 col-md-8 mb-4">
-                        <dashboard-card></dashboard-card>
+                        <dashboard-card dashboard-icon="fa-door-closed">
+                            <template slot="title">
+                                DND Rooms
+                            </template>
+                        </dashboard-card>
                     </div>
                 </div>
             </div>
