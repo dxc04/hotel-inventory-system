cb9d6ec3ea6602f6535308c1ed734f16164fff9b
Dixie Philamerah Atay <dixie.atay@codingavenue.com>
2019-05-04 22:14:23 +0800

    Add Room sales list

---
+++ resources/js/components/rooms/RoomTransactions.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="row room-transaction-list">
+        <div class="col-4">
+            <div class="list-group" id="list-tab" role="tablist">
+                <a v-for="room in rooms" class="list-group-item list-group-item-action" :href="getLink(room.id)" data-toggle="list" role="tab">
+                    <span>{{ room.room_name }}</span>
+                    <span v-if="getSalesCount(room.id)" class="badge badge-primary badge-pill">{{ getSalesCount(room.id) }}</span>
+                </a>
+            </div>
+        </div>
+        <div class="col-8">
+            <div class="tab-content" id="nav-tabContent">
+                <div v-for="room in rooms" :id="getRoomId(room.id)" class="tab-pane fade" role="tabpanel" aria-labelledby="list-profile-list">
+                    <transactions :room_id="room.id"></transactions>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Transactions from './Transactions.vue'
+    import { mapGetters, mapActions } from 'vuex'
+
+    export default {
+        name: 'RoomTransactions',
+        props: {
+            rooms: { 
+                type: Array,
+                required: true
+            },
+            transactions: {
+                type: Array
+            }
+        },
+        components: {
+            Transactions
+        },
+        computed: {
+            ...mapGetters({
+                roomsData: 'getRoomsData'
+            })
+        },
+        methods: {
+            getLink(id) {
+                return '#room_' + id
+            },
+            getRoomId(id) {
+                return 'room_' + id
+            },
+            getSalesCount(id) {
+                let sales = this.roomsData.sales.filter(sale => sale.room_id == id)
+
+                return sales.length
+            }
+        },
+        mounted() {
+            console.log(this.roomsData)
+        }
+    }
+</script>
+
+<style scoped>
+    .row {
+        margin-top: 30px;
+    }
+    .badge {
+        float: right;
+    }
+</style>
--- resources/js/pages/RoomsPage.vue
+++ resources/js/pages/RoomsPage.vue
@@ -1,10 +1,57 @@
 <template>
     <div class="container-fluid">
-        <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Rooms</h1>
+        <div class="card shadow mb-4">
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                 <h6 class="m-0 font-weight-bold">Floors</h6>
+            </div>
+            <div class="card-body">
+                <div class="row align-content-center align-items-center center">
+                    <div class="col-auto pb-2" v-for="floor in floors">
+                        <floor-box :floor="floor" @select-floor="selectFloor" :floorSelected="floorSelected(floor)"></floor-box>
+                    </div>
+                </div>
+                <span v-if="selectedFloor">
+                    <room-transactions :rooms="roomsBySelectedFloor"></room-transactions>
+                </span>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import FloorBox from '../components/check_rooms/FloorBox.vue'
+    import RoomTransactions from '../components/rooms/RoomTransactions.vue'
+    import { mapGetters, mapActions } from 'vuex'
+
+    export default {
+        name: 'RoomsPage',
+        components: {
+            FloorBox,
+            RoomTransactions
+        },
+        data() {
+            return {
+                selectedFloor: null
+            }
+        },
+        computed: {
+            ...mapGetters({
+                roomsData: 'getRoomsData'
+            }),
+            floors() {
+                return this.roomsData.floors
+            },
+            roomsBySelectedFloor() {
+                return this.roomsData.rooms.filter(room => room.floor_id == this.selectedFloor.id)
+            },
+        },
+        methods: {
+            selectFloor(floor) {
+                this.selectedFloor = floor
+            },
+            floorSelected(floor) {
+                return this.selectedFloor ? (this.selectedFloor.id == floor.id) : false
+            }                              
+        }
+    }
 </script>
